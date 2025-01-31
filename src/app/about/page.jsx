e6cc953f112b086fa377c96db9e5746b475796b4
Moniruzzaman-Monir <ad.monir2001@gmail.com>
2025-01-31 16:56:06 +0600

    Refactor About page component to include navigation and improve layout

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,11 +1,23 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
 import React from 'react';
 
-const page = () => {
-    return (
-        <div>
-            About page
-        </div>
-    );
+const AboutPage = () => {
+  const router = useRouter();
+  return (
+    <div>
+      <h1 className="font-semibold text-center text-4xl my-3 py-4">
+        About Page
+      </h1>
+      <button
+        onClick={() => router.push('/')}
+        className="border p-2 rounded-lg font-semibold text-lg"
+      >
+        Back to the Home
+      </button>
+    </div>
+  );
 };
 
-export default page;
+export default AboutPage;
